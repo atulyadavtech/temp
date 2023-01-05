f6b49b85d3504adff15c93f326ec8bd0ff688b38
Atul Yadav <atulyadavtech@gmail.com>
2023-01-05 16:19:30 +0530

    Create test.sh

---
+++ ras-aul/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export COMPARTMENT_ID=$(cat /root/.scripts/compid.out)
+export VCN_ID=$(oci network vcn list --compartment-id="$COMPARTMENT_ID" | grep  vcn | head -n 1 | cut -d '"' -f4)
+export SB=$(oci network subnet list --compartment-id=$COMPARTMENT_ID --vcn-id=$VCN_ID | grep -A11  public_subnet | tail -n 1 | cut -d '"' -f2)
+oci network security-list create --compartment-id $COMPARTMENT_ID --vcn-id $VCN_ID --display-name aul-ras-rule --ingress-security-rules file:///root/ras-aul/ingress-aul-ras.json  --egress-security-rules file:///root/ras-aul/egress-aul-ras.json
