0e6a5b8fc088b072c7221640bcbff0f8e86c60a3
Atul Yadav <atulyadavtech@gmail.com>
2023-01-05 16:50:22 +0530

    Update 2.sh

--- ras-aul/2.sh
+++ ras-aul/2.sh
@@ -1,4 +1,4 @@
 export COMPARTMENT_ID=$(cat /root/.scripts/compid.out)
 export VCN_ID=$(oci network vcn list --compartment-id="$COMPARTMENT_ID" | grep  vcn | head -n 1 | cut -d '"' -f4)
-export SL=$(oci network security-list list  --compartment-id=$COMPARTMENT_ID --vcn-id=$VCN_ID --output table --query 'data [*].{Name:"display-name", id:id}' | grep -i aul-ras-rule | head -n 1 |  cut -d '|' -f3)
+export SL=$(oci network security-list list  --compartment-id=$COMPARTMENT_ID --vcn-id=$VCN_ID --output table --query 'data [*].{Name:"display-name", id:id}' | grep -i aul-ras-rule )
 echo $SL > /root/ras-aul/sl.out
