bf8c50fed388d02ecfb17f610630c01f0988bfe1
Atul Yadav <atulyadavtech@gmail.com>
2023-01-23 18:03:44 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-cp -rfv /altair/pbsworks/pbsaccess/home/config/api_gateway/certs /altair/pbsworks/pbsaccess/home/config/api_gateway/certs-$(date +%Y%m%d%H%M%S)
-cd /altair/pbsworks/pbsaccess/home/config/api_gateway/certs/
-rm -f wcard[0-9].*
-/usr/bin/wget https://hwulvatest1.blob.core.windows.net/arm/wcard2.key > /dev/null 2>&1
-/usr/bin/wget https://hwulvatest1.blob.core.windows.net/arm/wcard2.crt > /dev/null 2>&1
+
 yes|cp -f wcard2.key wcard.key
 yes|cp -f wcard2.crt wcard.crt
 chmod 775 wcard.*
