4445978a0a23a1c08c3000dcc0743185dc4a7609
Atul Yadav <atulyadavtech@gmail.com>
2025-02-06 12:23:51 +0530

    Update tag.sh

--- tag.sh
+++ tag.sh
@@ -1,2 +1,2 @@
 export COMPARTMENT_ID=$(cat /root/.scripts/compid.out)
-/usr/bin/oci iam compartment update  --compartment-id $COMPARTMENT_ID --freeform-tags '{"isProduction": "activeFeb2025"}' --force
+oci iam compartment update  --compartment-id $COMPARTMENT_ID --freeform-tags '{"isProduction": "activeFeb2025"}' --force
